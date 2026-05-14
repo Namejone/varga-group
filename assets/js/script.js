09c506edf528784a56fc093d5a45f7c0b0fa75b2
Namejone <devnamejone@gmail.com>
2026-05-14 16:30:38 +0600

    Biddut gece.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,63 @@ const heroSwiper = new Swiper('.hero__slider', {
     },
     speed: 800,
 });
+const brandsSliderLeft = new Swiper('.brands__dir-left', {
+    slidesPerView: "auto",
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+    },
+    speed: 4000,
+    allowTouchMove: false,
+    breakpoints: {
+        480: {
+            spaceBetween: 50
+        },
+        768: {
+            spaceBetween: 70
+        },
+        1024: {
+            spaceBetween: 120
+        }
+    }
+});
+const brandsSliderRight = new Swiper('.brands__dir-right', {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false,
+        reverseDirection: true,
+    },
+    speed: 4000,
+    allowTouchMove: false,
+    breakpoints: {
+        480: {
+            spaceBetween: 50
+        },
+        768: {
+            spaceBetween: 70
+        },
+        1024: {
+            spaceBetween: 120
+        }
+    }
+});
+
+const testimonialSwiper = new Swiper('.testimonial__slider', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed: 800,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
 
 // Offcanvas menu script
 document.addEventListener('DOMContentLoaded', () => {
