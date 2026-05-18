3c7b01921abb8367b9ab1182a2c6b68d368f938e
Namejone <devnamejone@gmail.com>
2026-05-18 17:14:28 +0600

    Script button animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,3 +87,105 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+
+
+gsap.registerPlugin(ScrollTrigger);
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 767px)", () => {
+    const tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".pin-section",
+            start: "top top",
+            end: "+=2500",
+            pin: true,
+            scrub: 1,
+        }
+    });
+
+    tl.to(".center-item", {
+        width: "100vw",
+        height: "100vh",
+        duration: 1.5,
+        ease: "none"
+    }, "start")
+        .to(".main-img-wrap", {
+            borderRadius: 0,
+            duration: 1.5
+        }, "start")
+        .to(".content-overlay", {
+            opacity: 1,
+            y: 0,
+            duration: 0.5,
+            ease: "power2.out"
+        }, ">-0.8")
+        .to({}, { duration: 0.2 });
+
+    return () => {
+        if (tl.scrollTrigger) tl.scrollTrigger.kill();
+        tl.kill();
+    };
+});
+
+
+
+// Service hero Animations
+const tl = gsap.timeline({ defaults: { ease: "power2.out" } });
+tl.to(".left", {
+    x: 0,
+    opacity: 1,
+    duration: 1.2
+})
+    .to(".right", {
+        x: 0,
+        opacity: 1,
+        duration: 1.2
+    }, "<")
+
+    .to(".main-circle", {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        ease: "back.out(1.2)"
+    }, "-=0.5").to(".ring-container", {
+        opacity: 1,
+        duration: 0.5,
+        ease: "linear"
+    }, "-=0.3");
+
+
+// Testimonial Section Animation
+gsap.registerPlugin(ScrollTrigger);
+const testimonialtl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".testimonial__preloader",
+        start: "top 80%",
+        toggleActions: "play none none none"
+    }
+});
+
+testimonialtl.from(".testimonial-preloader-content h2", {
+    y: -50,
+    opacity: 0,
+    duration: 1,
+    ease: "power2.out"
+})
+    .from(".testimonial-preloader-content p", {
+        y: 50,
+        opacity: 0,
+        duration: 1,
+        ease: "power2.out"
+    }, "-=0.5")
+    .to(".borderAnimation", {
+        "--border-width": "100%",
+        duration: 0.8,
+        ease: "power1.inOut"
+    })
+    .to(".testimonial__preloader", {
+        opacity: 0,
+        autoAlpha: 0,
+        duration: 0.5,
+        delay: 1
+    });
